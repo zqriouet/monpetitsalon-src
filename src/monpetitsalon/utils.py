90a3d7821c4ec1ce7d376320f8e7e6e162426b44
zqriouet <zqriouet@gmail.com>
2024-03-24 18:34:29 +0100

    minor changes

--- src/monpetitsalon/utils.py
+++ src/monpetitsalon/utils.py
@@ -2,8 +2,7 @@ import re
 import unicodedata
 import logging
 from bs4 import BeautifulSoup
-# from selenium import webdriver
-from seleniumwire import webdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
